9f3dd31bc1c772256431dfb3ded0449ed599c3c7
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-09-28 14:06:34 +0600

    header footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Created By Shafi Rayhan</p>
+      <div className="social-links">
+        <p>Cool</p>
+        <p>Cool</p>
+        <p>Cool</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/shafi.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+ReactDOM.render(
+  <div className="container">
+    <Header />
+    <Footer />
+  </div>,
+  document.getElementById('root')
+);
