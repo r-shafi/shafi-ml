b64c2e8d697c77d1fc51b340dae791b8f3792a35
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-10-14 13:42:51 +0600

    Router Fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
@@ -11,7 +10,17 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Router>
+          <header>
+            <div>
+              <h1>Shafi</h1>
+              <Link to="/">Home</Link>
+              <Link to="/blog">Blog</Link>
+              <Link to="/contact">Contact</Link>
+            </div>
+          </header>
+        </Router>
+
         <div className="container">
           <Router>
             <Switch>
