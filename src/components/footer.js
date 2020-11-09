5f53df67421d93673426165c0fe05249b30a43e8
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-11-09 18:52:18 +0600

    minor style changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import twitter from '../media/twitter.svg';
+import whatsapp from '../media/whatsapp.svg';
+import facebook from '../media/facebook.svg';
+import messenger from '../media/messenger.svg';
+
 export default function Footer() {
   return (
     <footer>
-      <p>Created by Shafi</p>
-      <p>&copy; {new Date().getFullYear()}</p>
+      <div className="text">
+        <p>Created by Shafi Rayhan</p>
+        <p>&copy; {new Date().getFullYear()}</p>
+      </div>
+      <div className="social">
+        <a href="https://twitter.com/shafiemoji">
+          <img src={twitter} alt="twitter profile" />
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=8801704792392">
+          <img src={whatsapp} alt="" />
+        </a>
+        <a href="https://www.facebook.com/shafiemoji">
+          <img src={facebook} alt="" />
+        </a>
+        <a href="https://m.me/shafiemoji">
+          <img src={messenger} alt="" />
+        </a>
+      </div>
     </footer>
   );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import Base from '../components/base';
 
-export default function Contact() {
+function About() {
+  return (
+    <div className="about">
+      <img src="https://picsum.photos/200" alt="profile image" />
+      <h2>Shafi Rayhan</h2>
+      <p>Front End Web Developer</p>
+      <p>
+        Studying Power Engineering at <em>Sylhet Polytechnic Institute</em>
+      </p>
+      <p>Placeholder text incoming...</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+        ratione. Amet dolores repellat qui iste, autem minus expedita quo
+        incidunt!
+      </p>
+    </div>
+  );
+}
+
+function ContactForm() {
+  return (
+    <main className="form">
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" />
+        <label htmlFor="msg">Message:</label>
+        <textarea id="msg"></textarea>
+        <button type="submit" id="send">
+          Send
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default function ContactPage() {
   return (
     <Base>
       <div className="contact">
-        <div className="about">
-          <img src="https://picsum.photos/200" alt="profile image" />
-          <h2>NAME NAME ABC</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In, earum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum fuga
-            officia dolor atque, distinctio placeat cumque molestias veritatis
-            praesentium ad.
-          </p>
-        </div>
-        <main className="form">
-          <form>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" />
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" />
-            <label htmlFor="msg">Message:</label>
-            <textarea id="msg"></textarea>
-            <button type="submit" id="send">
-              Send
-            </button>
-          </form>
-        </main>
+        <About />
+        <ContactForm />
       </div>
     </Base>
   );
