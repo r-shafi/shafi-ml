83ad0486d45b507b5c8788647113ba494e06add3
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-11-11 16:32:27 +0600

    form validation

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Shafi\\Documents\\Codes\\shafi-ml\\.cache\\dev-404-page.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\Shafi\\Documents\\Codes\\shafi-ml\\src\\pages\\contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Shafi\\Documents\\Codes\\shafi-ml\\src\\pages\\index.js"))),
+  "component---src-pages-works-js": hot(preferDefault(require("C:\\Users\\Shafi\\Documents\\Codes\\shafi-ml\\src\\pages\\works.js")))
+}
+
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ import '../styles/contact.css';
 function About() {
   return (
     <div className="about">
-      <img src="https://picsum.photos/200" alt="profile image" />
+      <img src="https://picsum.photos/200" alt="profile" />
       <h2>Shafi Rayhan</h2>
       <p>Front End Web Developer</p>
       <p>
@@ -22,22 +22,56 @@ function About() {
   );
 }
 
-function ContactForm() {
-  return (
-    <main className="form">
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" />
-        <label htmlFor="msg">Message:</label>
-        <textarea id="msg"></textarea>
-        <button type="submit" id="send">
-          Send
-        </button>
-      </form>
-    </main>
-  );
+class ContactForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      msg: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!this.state.email.match(emailRegex)) {
+      e.preventDefault();
+      document.querySelector('.error').style.display = 'block';
+      setTimeout(() => {
+        document.querySelector('.error').style.display = 'none';
+      }, 3000);
+    }
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+
+  render() {
+    return (
+      <main className="form">
+        <form onSubmit={this.handleSubmit} name="message" netlify>
+          <label htmlFor="name">Name:</label>
+          <input required type="text" id="name" onChange={this.handleChange} />
+          <label htmlFor="email">Email:</label>
+          <input
+            required
+            type="email"
+            id="email"
+            onChange={this.handleChange}
+          />
+          <p className="error">Not a valid email</p>
+          <label htmlFor="msg">Message:</label>
+          <textarea required id="msg" onChange={this.handleChange}></textarea>
+          <button type="submit" id="send">
+            Send
+          </button>
+        </form>
+      </main>
+    );
+  }
 }
 
 export default function ContactPage() {
