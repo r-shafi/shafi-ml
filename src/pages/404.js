0d5f8f36f8f3cd0960397873cafa4f7713b8fdf2
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-12-22 13:39:37 +0600

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,15 @@ import React from 'react';
 import Base from '../components/base';
 import Head from '../components/head';
 
+import four from '../media/404.svg';
+
 export default function FourOFour() {
   return (
     <Base>
       <Head title="404" />
-      <h1>404 | Not Found</h1>
+      <div className="fourimage">
+        <img src={four} alt="" />
+      </div>
     </Base>
   );
 }
