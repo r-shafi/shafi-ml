efb3c760e721826eecf4c0b3529a09b66e41996f
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-12-22 11:14:12 +0600

    design fixes

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,1 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":1000,"linkImagesToOriginal":true,"showCaptions":false,"markdownCaptions":false,"sizeByPixelDensity":false,"backgroundColor":"white","quality":50,"withWebp":false,"tracedSVG":false,"loading":"lazy","disableBgImageOnAlpha":false,"disableBgImage":false},
-    }]
+module.exports = []
