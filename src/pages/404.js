a5e2353456aaac988b5408953ed30ab9c2ec4d49
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-12-24 13:05:35 +0600

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,12 +5,14 @@ import Head from '../components/head';
 
 import four from '../media/404.svg';
 
+import style from '../style/error.module.css';
+
 export default function FourOFour() {
   return (
     <Base>
       <Head title="404" />
-      <div className="fourimage">
-        <img src={four} alt="" />
+      <div className={style.error}>
+        <img src={four} alt="Page not found" />
       </div>
     </Base>
   );
