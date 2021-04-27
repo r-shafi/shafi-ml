fdddf7cb55e1f9f062fdbd29d5d4ecba3082a5b8
Shafi Rayhan <rayhanshafi7@gmail.com>
2021-04-28 03:11:59 +0600

    twitter card error fix

--- src/components/head.js
+++ src/components/head.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import twitter from '../assets/twitter.svg';
 
 const defaultDescription = 'I am Shafi Rayhan, an experienced Front-End Web Developer from Sylhet, Bangladesh.';
 const defaultKW = 'Front-End, Web Developer, Sylhet, React, GatsbyJS';
@@ -16,7 +17,9 @@ export default function Head({ title, description, keywords }) {
         {' '}
         | Shafi Rayhan
       </title>
+      <meta property="og:type" content="website"></meta>
       <meta name="twitter:card" content="summary"></meta>
+      <meta name="twitter:image" content={twitter}></meta>
       <meta name="twitter:creator" content="@shafiemoji"></meta>
       <meta name="twitter:title" content={title}></meta>
       <meta name="twitter:description" content={description || defaultDescription}></meta>
