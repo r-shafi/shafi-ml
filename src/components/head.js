d4f084f427481d40a62941e93919ef1741397906
Shafi Rayhan <rayhanshafi7@gmail.com>
2021-04-28 13:52:39 +0600

    favicon

--- src/components/head.js
+++ src/components/head.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import favicon from '../../static/favicon.png'
 
 const defaultDescription = 'I am Shafi Rayhan, an experienced Front-End Web Developer from Sylhet, Bangladesh.';
 const defaultKW = 'Front-End, Web Developer, Sylhet, React, GatsbyJS';
@@ -16,6 +17,8 @@ export default function Head({ title, description, keywords }) {
         {' '}
         | Shafi Rayhan
       </title>
+
+      <link rel="shortcut icon" href={favicon} type="image/png"/>
       
       <meta property="og:title" content={title} />
       <meta property="og:type" content="article" />
@@ -23,11 +26,12 @@ export default function Head({ title, description, keywords }) {
       <meta property="og:image" content="https://i.imgur.com/Sk32f3O.png" />
       <meta property="og:description" content={description || defaultDescription} />
 
-      <meta name="twitter:card" content="summary"></meta>
-      <meta name="twitter:image" content="https://i.imgur.com/Sk32f3O.png"></meta>
-      <meta name="twitter:creator" content="@shafiemoji"></meta>
-      <meta name="twitter:title" content={title}></meta>
-      <meta name="twitter:description" content={description || defaultDescription}></meta>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@nytimes" />
+      <meta name="twitter:creator" content="@SarahMaslinNir" />
+      <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral" />
+      <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here." />
+      <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg" />
     </Helmet>
   );
 }
